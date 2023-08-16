94fd03e2f1beb8a1e55643580ff068683e8f0072
singhpalwinder <palwinders969@gmail.com>
2023-08-16 16:47:36 -0500

    made changes to background image and other css properties

--- links/index.php
+++ links/index.php
@@ -8,37 +8,37 @@
   <body>
         <div class = "container">
 
-          <div class = "img">
-            <a href = "https://www.singhpalwinder.com"><img src = "pfp.png" alt = "Profile picture" class = "img"></a>
+          <div>
+            <a href = "https://www.singhpalwinder.com"><img class="pfp" src = "pfp.png" alt = "Profile picture"></a>
           </div>
 
           <div class = "socials">
-            <div>
-              <a class = "icon" href="https://www.twitter.com/_singhpalwinder"><img src="images/twitter-icon.svg" alt="Twitter Icon"></a>
+            <div  class="icon">
+              <a   href="https://www.twitter.com/_singhpalwinder"><img src="images/twitter-icon.svg" alt="Twitter Icon"></a>
             </div>
             <div>
-              <a class = "icon" href ="https://github.com/singhpalwinder"><i class="fa-brands fa-github"></i></a>
+              <a class="icon" href ="https://github.com/singhpalwinder"><i class="fa-brands fa-github"></i></a>
             </div>
             <div>
-            <a class = "icon" href="https://www.linkedin.com/in/palwinder-singh-432559218"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a  class="icon" href="https://www.linkedin.com/in/palwinder-singh-432559218"><i class="fa-brands fa-linkedin-in"></i></a>
             </div>
             <div>
-            <a class = "icon"  href="https://www.youtube.com/channel/UC3_SwfcrJEg1CpHlVMRY_Xw"><i class="fa-brands fa-youtube"></i></a>
+            <a  class="icon" href="https://www.youtube.com/channel/UC3_SwfcrJEg1CpHlVMRY_Xw"><i class="fa-brands fa-youtube"></i></a>
             </div>
             <div>
-            <a class = "icon"  href="https://www.instagram.com/_singhpalwinder"><i class = "fa-brands fa-instagram"></i></a>
+            <a  class="icon" href="https://www.instagram.com/_singhpalwinder"><i class = "fa-brands fa-instagram"></i></a>
             </div>
             <div>            
-              <a class = "icon"  href="https://www.tiktok.com/@_singhpalwinder"><i class = "fa-brands fa-tiktok"></i></a>
+              <a class="icon"  href="https://www.tiktok.com/@_singhpalwinder"><i class = "fa-brands fa-tiktok"></i></a>
             </div>
           </div>
 
           
             <div class = "links">
-              <div class = "mediaLinks">
+              <div>
                 <a class = "button" href = "https://music.apple.com/us/playlist/multi-cultural-gym-hype/pl.u-yZyVJVxFqBY37a">Gym Playlist</a>
               </div>
-              <div class = "mediaLinks">
+              <div>
                 <a class = "button" href = "20wk_powerbuilding.numbers">Free Workout Plan</a>
               </div>
 
