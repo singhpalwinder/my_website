f1658c7e4c57e2bd2101523efa40d4fefafd106d
singhpalwinder <palwinders969@gmail.com>
2023-08-19 18:13:20 -0500

    number of plates now show with the color of the plate as the bullet point

--- links/Calculate_plates/plates_calculator.php
+++ links/Calculate_plates/plates_calculator.php
@@ -1,8 +1,51 @@
+<?php
+// Start the session at the top
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['weight_in_lbs'])) {
+    function platesNeeded($target_weight_lbs) {
+        $lb_to_kg = 0.453592;
+        $target_weight_kg = $target_weight_lbs * $lb_to_kg;
+
+        $results = [
+            'weight_lbs' => number_format($target_weight_lbs, 2),
+            'weight_kg' => number_format($target_weight_kg, 2)
+        ];
+
+        $weight_without_barbell = $target_weight_kg - 20;
+        $plates = [25, 20, 15, 10, 5, 2.5, 1.25];
+        rsort($plates);
+
+        $plates_needed = [];
+        foreach($plates as $plate) {
+            while ($weight_without_barbell >= 2 * $plate) {
+                if (!isset($plates_needed[strval($plate)])) {
+                    $plates_needed[strval($plate)] = 0;
+                }
+                $plates_needed[strval($plate)]++;
+                $weight_without_barbell -= 2 * $plate; // For both sides
+            }
+        }
+    
+
+        $results['plates_needed'] = $plates_needed;
+        return $results;
+    }
+
+    $weight_in_lbs = floatval($_POST['weight_in_lbs']);
+    $_SESSION['results'] = platesNeeded($weight_in_lbs);
+    
+    // Redirect back to the form page
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href= "styling.css">
+    <link rel="stylesheet" href="styling.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weight Conversion & Denomination</title>
 </head>
@@ -13,45 +56,23 @@
     </form>
 
     <?php
+    if (isset($_SESSION['results'])) {
+        $results = $_SESSION['results'];
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['weight_in_lbs'])) {
-        function platesNeeded($target_weight_lbs) {
-            $lb_to_kg = 0.453592;
-            $target_weight_kg = $target_weight_lbs * $lb_to_kg;
-            
-            echo "<h3>Given Weight(lbs): " . number_format($target_weight_lbs, 2) . "lbs</h3>";
-            echo "<h3>Target weight in kg: " . number_format($target_weight_kg, 2) . "kg</h3>";
+        echo "<h3>Given Weight(lbs): " . $results['weight_lbs'] . "lbs</h3>";
+        echo "<h3>Target weight in kg: " . $results['weight_kg'] . "kg</h3>";
 
-            $weight_without_barbell = $target_weight_kg - 20;
-
-            $plates = [25, 20, 15, 10, 5, 2.5, 1.25];
-            rsort($plates);
-
-            $plates_needed = [];
-
-            foreach($plates as $plate) {
-                if ($weight_without_barbell >= 2 * $plate) {
-                    $num_plates = floor($weight_without_barbell / (2 * $plate));
-                    $plates_needed[strval($plate)] = $num_plates;
-                    $weight_without_barbell -= $num_plates * 2 * $plate;
-                }
-            }
-
-            return $plates_needed;
-        }
-
-        $weight_in_lbs = floatval($_POST['weight_in_lbs']);
-        $result = platesNeeded($weight_in_lbs);
-        
         echo "<h4>Plates needed on each side:</h4>";
         echo "<ul>";
-        foreach($result as $plate => $count) {
-            echo "<li>" . floatval($plate) . " kg plates: $count</li>";
+        foreach($results['plates_needed'] as $plate => $count) {
+            $class_name = 'plate-' . str_replace('.', '_', $plate);
+            echo "<li class='" . $class_name . "'>" . floatval($plate) . " kg plates: $count</li>";
         }
-        
         echo "</ul>";
-    }
 
+        // Clear the session data
+        unset($_SESSION['results']);
+    }
     ?>
 </body>
 </html>
