2a1c28e1a94c9ab45809a9a55da88354f4e95808
singhpalwinder <palwinders969@gmail.com>
2023-08-19 19:07:54 -0500

    made directory changes for fitness tools

--- links/index.php
+++ links/index.php
@@ -45,7 +45,7 @@
                   <a class = "button" href = "20wk_powerbuilding.numbers">Free Workout Plan</a>
                 </div>
                 <div>
-                  <a class = "button" href = "Calculate_plates/plates_calculator.php">Weight Calculator</a>
+                  <a class = "button" href = "../fitness/tools/Calculate_plates/">Weight Calculator</a>
                 </div>
             </div>
 
