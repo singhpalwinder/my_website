e1462a4d18b04226b807860482695dea5fdc3594
Paul Singh <palwinders969@gmail.com>
2024-10-02 22:20:12 -0500

    fixed content overflow

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styling.css">
+    <link rel="stylesheet" href="styling.css">
     <script src="https://kit.fontawesome.com/05714a92a3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Links Page</title>
