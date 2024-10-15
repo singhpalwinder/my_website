feb7ab313b8c3d2eb0e95fbe6a59af6160f5c78f
Paul Singh <palwinders969@gmail.com>
2024-10-15 13:59:16 -0500

    cleaned up css and html

--- index.php
+++ index.php
@@ -5,54 +5,37 @@
     <link rel="stylesheet" href="styling.css">
     <script src="https://kit.fontawesome.com/05714a92a3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <title>Links Page</title>
+    <title>Links</title>
   </head>
   <body>
-    <div class="background"></div> <!-- Background layer -->
-
-    <div class="content"> <!-- Content layer -->
-      <header>
+     
+     <header>
           <img src="pfp.png" alt="Profile picture">
           <a href="https://www.singhpalwinder.com/">@_singhpalwinder</a>
       </header>
 
       <div class="links">
-        <div>
           <a class="button" href="./fitness/Calculate_plates/plates_calculator.php">
-            <div class="button-image">
               <img src="images/plates_calculator.png" alt="Plates Calculator">
-            </div>
             <span class="button-text">Plates Calculator</span>
           </a>
-        </div>
-        <div>
           <a class="button" href="20wk_powerbuilding.numbers">
-            <div class="button-image">
               <img src="images/free_workout_plan.png" alt="Free Workout Plan">
-            </div>
             <span class="button-text">Free Workout Plan</span>
           </a>
-        </div>
-        <div>
           <a class="button" href="https://music.apple.com/us/playlist/multi-cultural-gym-hype/pl.u-yZyVJVxFqBY37a">
-            <div class="button-image">
               <img src="images/playlist_logo.png" alt="Gym Playlist">
-            </div>
             <span class="button-text">Gym Playlist</span>
           </a>
-        </div>
       </div>
 
       <footer>
         <div class="icon">
-          <a href="https://www.twitter.com/_singhpalwinder"><i class="fa-brands fa-x-twitter"></i></a>
           <a href="https://github.com/singhpalwinder"><i class="fa-brands fa-github"></i></a>
           <a href="https://www.linkedin.com/in/singhpalwinder1/"><i class="fa-brands fa-linkedin-in"></i></a>
-          <a href="https://www.instagram.com/_singhpalwinder"><i class="fa-brands fa-instagram"></i></a>
-          <a href="https://www.tiktok.com/@_singhpalwinder"><i class="fa-brands fa-tiktok"></i></a>
         </div>
-        <div><h3>Self hosted by Palwinder Singh</h3></div>
+        <h5>Self hosted by Palwinder Singh</h5>
       </footer>
-    </div>
+     
   </body>
 </html>
