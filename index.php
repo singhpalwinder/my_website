45b82a90b91506c057b7dd6935d4fb9a0dc4cc52
Paul Singh <palwinders969@gmail.com>
2024-10-27 14:28:15 -0500

    made container changes

--- index.php
+++ index.php
@@ -9,31 +9,33 @@
   </head>
   <body>
      
-     <header>
-          <img src="pfp.png" alt="Profile picture">
-          <a href="https://www.singhpalwinder.com/">@_singhpalwinder</a>
-      </header>
-      <div class="links">
-          <a class="button" href="./fitness/Calculate_plates/plates_calculator.php">
-              <img src="images/plates_calculator.png" alt="Plates Calculator">
-            <span class="button-text">Plates Calculator</span>
-          </a>
-          <a class="button" href="20wk_powerbuilding.numbers">
-              <img src="images/free_workout_plan.png" alt="Free Workout Plan">
-            <span class="button-text">Free Workout Plan</span>
-          </a>
-          <a class="button" href="https://music.apple.com/us/playlist/multi-cultural-gym-hype/pl.u-yZyVJVxFqBY37a">
-              <img src="images/playlist_logo.png" alt="Gym Playlist">
-            <span class="button-text">Gym Playlist</span>
-          </a>
-      </div>
-      <footer>
-        <div class="icon">
-          <a href="https://github.com/singhpalwinder"><i class="fa-brands fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/singhpalwinder1/"><i class="fa-brands fa-linkedin-in"></i></a>
-        </div>
-        <h5>Self hosted by Palwinder Singh</h5>
-      </footer>
+    <div class="content">
+          <header>
+              <img src="pfp.png" alt="Profile picture">
+              <a href="https://www.singhpalwinder.com/">@_singhpalwinder</a>
+          </header>
+          <div class="links">
+              <a class="button" href="./fitness/Calculate_plates/plates_calculator.php">
+                  <img src="images/plates_calculator.png" alt="Plates Calculator">
+                <span class="button-text">Plates Calculator</span>
+              </a>
+              <a class="button" href="20wk_powerbuilding.numbers">
+                  <img src="images/free_workout_plan.png" alt="Free Workout Plan">
+                <span class="button-text">Free Workout Plan</span>
+              </a>
+              <a class="button" href="https://music.apple.com/us/playlist/multi-cultural-gym-hype/pl.u-yZyVJVxFqBY37a">
+                  <img src="images/playlist_logo.png" alt="Gym Playlist">
+                <span class="button-text">Gym Playlist</span>
+              </a>
+          </div>
+          <footer>
+            <div class="icon">
+              <a href="https://github.com/singhpalwinder"><i class="fa-brands fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/singhpalwinder1/"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <h5>Self hosted by Palwinder Singh</h5>
+          </footer>
+    </div>
      
   </body>
 </html>
